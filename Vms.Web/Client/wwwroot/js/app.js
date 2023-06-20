a91fa9480819d290fc6b1de630f4ad3cf7be6253
Mark Baldwin <markb@utopiasoftware.co.uk>
2023-06-20 18:25:11 +0100

    x

--- Vms.Web/Client/wwwroot/js/app.js
+++ Vms.Web/Client/wwwroot/js/app.js
@@ -218,6 +218,129 @@ window.dropdownButton = {
     }
 };
 
+window.selectOnlyCombobox = {
+    installOnComboKeyDown: function (id, objRef) {
+        const element = document.getElementById(id);
+        if (!element)
+            return false;
+
+        const SelectActions = {
+            Close: 0,
+            CloseSelect: 1,
+            First: 2,
+            Last: 3,
+            Next: 4,
+            Open: 5,
+            PageDown: 6,
+            PageUp: 7,
+            Previous: 8,
+            Select: 9,
+            Type: 10,
+        };
+
+        element.addEventListener('keydown', (event) => {
+
+            const { key } = event;
+
+            const isOpen = element.getAttribute('aria-activedescendant').length > 0;
+
+            const action = getActionFromKey(event, isOpen);
+
+            switch (action) {
+                case SelectActions.Last:
+                case SelectActions.First:
+                    objRef.invokeMethodAsync("updateMenuStateJS", true);
+
+                // intentional fallthrough
+                case SelectActions.Next:
+                case SelectActions.Previous:
+                case SelectActions.PageUp:
+                case SelectActions.PageDown:
+                    event.preventDefault();
+                    objRef.invokeMethodAsync("onOptionChangeJS", action);
+                    scrollActiveItemIntoView(element.getAttribute('aria-activedescendant'));
+                    return;
+
+                case SelectActions.CloseSelect:
+                    event.preventDefault();
+                    objRef.invokeMethodAsync("selectOptionJS");
+
+                // intentional fallthrough
+                case SelectActions.Close:
+                    event.preventDefault();
+                    objRef.invokeMethodAsync("updateMenuStateJS", false);
+                    return;
+                case SelectActions.Type:
+                    objRef.invokeMethodAsync("onComboTypeJS", key).then(activeId => {
+                        scrollActiveItemIntoView(activeId);
+                    });
+                    return;
+
+                case SelectActions.Open:
+                    event.preventDefault();
+                    objRef.invokeMethodAsync("updateMenuStateJS", true);
+                    return;
+            }
+
+            function scrollActiveItemIntoView(activeId) {
+                if (activeId) {
+                    const el = document.getElementById(activeId);
+                    if (el)
+                        el.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
+                }
+            }
+
+            function getActionFromKey(event, menuOpen) {
+                const { key, altKey, ctrlKey, metaKey } = event;
+                const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ' ']; // all keys that will do the default open action
+
+                // handle opening when closed
+                if (!menuOpen && openKeys.includes(key)) {
+                    return SelectActions.Open;
+                }
+
+                // home and end move the selected option when open or closed
+                if (key === 'Home') {
+                    return SelectActions.First;
+                }
+                if (key === 'End') {
+                    return SelectActions.Last;
+                }
+
+                // handle typing characters when open or closed
+                if (
+                    key === 'Backspace' ||
+                    key === 'Clear' ||
+                    (key.length === 1 && key !== ' ' && !altKey && !ctrlKey && !metaKey)
+                ) {
+                    return SelectActions.Type;
+                }
+
+                // handle keys when open
+                if (menuOpen) {
+                    if (key === 'ArrowUp' && altKey) {
+                        return SelectActions.CloseSelect;
+                    } else if (key === 'ArrowDown' && !altKey) {
+                        return SelectActions.Next;
+                    } else if (key === 'ArrowUp') {
+                        return SelectActions.Previous;
+                    } else if (key === 'PageUp') {
+                        return SelectActions.PageUp;
+                    } else if (key === 'PageDown') {
+                        return SelectActions.PageDown;
+                    } else if (key === 'Escape') {
+                        return SelectActions.Close;
+                    } else if (key === 'Enter' || key === ' ') {
+                        return SelectActions.CloseSelect;
+                    }
+                }
+            }
+        });
+
+        return true;
+    },
+}
+
 window.menuButton = {
     // keeps track of 'onmousedown' event listeners added to the window so they
     // can later be removed.
@@ -266,138 +389,7 @@ window.menuButton = {
         return true;
     },
 
-    maintainScrollVisibility: function (activeElement, scrollParent) {
-        const { offsetHeight, offsetTop } = activeElement;
-        const { offsetHeight: parentOffsetHeight, scrollTop } = scrollParent;
-
-        const isAbove = offsetTop < scrollTop;
-        const isBelow = offsetTop + offsetHeight > scrollTop + parentOffsetHeight;
-
-        if (isAbove) {
-            scrollParent.scrollTo(0, offsetTop);
-        } else if (isBelow) {
-            scrollParent.scrollTo(0, offsetTop - parentOffsetHeight + offsetHeight);
-        }
-    },
-
-    installOnComboKeyDown: function (id, objRef) {
-        const element = document.querySelector('#' + id);
-        console.log('installOnComboKeyDown() id: ', id, ' element: ', element, ' objRef: ', objRef);
-        if (!element) {
-            return false;
-        }
-
-        const SelectActions = {
-            Close: 0,
-            CloseSelect: 1,
-            First: 2,
-            Last: 3,
-            Next: 4,
-            Open: 5,
-            PageDown: 6,
-            PageUp: 7,
-            Previous: 8,
-            Select: 9,
-            Type: 10,
-        };
-
-        element.addEventListener('keydown', (event) => {
-
-            const { key } = event;
-
-            //var aad = element.getAttribute('aria-activedescendant');
-            const isOpen = element.getAttribute('aria-activedescendant').length > 0;
-
-            const action = getActionFromKey(event, isOpen);
-
-            switch (action) {
-                case SelectActions.Last:
-                case SelectActions.First:
-                    objRef.invokeMethodAsync("updateMenuStateJS", true);
-                
-                // intentional fallthrough
-                case SelectActions.Next:
-                case SelectActions.Previous:
-                case SelectActions.PageUp:
-                case SelectActions.PageDown:
-                    event.preventDefault();
-                    objRef.invokeMethodAsync("onOptionChangeJS", action);
-
-                    const activeElement = element.getAttribute('aria-activedescendant');
-                    if (activeElement) {
-                        const el = document.getElementById(activeElement);
-                        el.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
-                    }
-                    return;
-                
-                case SelectActions.CloseSelect:
-                    event.preventDefault();
-                    objRef.invokeMethodAsync("selectOptionJS");
-
-                // intentional fallthrough
-                case SelectActions.Close:
-                    event.preventDefault();
-                    objRef.invokeMethodAsync("updateMenuStateJS", false);
-                    return;
-                case SelectActions.Type:
-                    objRef.invokeMethodAsync("onComboTypeJS", key);
-                    return;
-                
-                case SelectActions.Open:
-                    event.preventDefault();
-                    objRef.invokeMethodAsync("updateMenuStateJS", true);
-                    return;
-            }
-
-
-            function getActionFromKey(event, menuOpen) {
-                const { key, altKey, ctrlKey, metaKey } = event;
-                const openKeys = ['ArrowDown', 'ArrowUp', 'Enter', ' ']; // all keys that will do the default open action
-                // handle opening when closed
-                if (!menuOpen && openKeys.includes(key)) {
-                    return SelectActions.Open;
-                }
-
-                // home and end move the selected option when open or closed
-                if (key === 'Home') {
-                    return SelectActions.First;
-                }
-                if (key === 'End') {
-                    return SelectActions.Last;
-                }
-
-                // handle typing characters when open or closed
-                if (
-                    key === 'Backspace' ||
-                    key === 'Clear' ||
-                    (key.length === 1 && key !== ' ' && !altKey && !ctrlKey && !metaKey)
-                ) {
-                    return SelectActions.Type;
-                }
-
-                // handle keys when open
-                if (menuOpen) {
-                    if (key === 'ArrowUp' && altKey) {
-                        return SelectActions.CloseSelect;
-                    } else if (key === 'ArrowDown' && !altKey) {
-                        return SelectActions.Next;
-                    } else if (key === 'ArrowUp') {
-                        return SelectActions.Previous;
-                    } else if (key === 'PageUp') {
-                        return SelectActions.PageUp;
-                    } else if (key === 'PageDown') {
-                        return SelectActions.PageDown;
-                    } else if (key === 'Escape') {
-                        return SelectActions.Close;
-                    } else if (key === 'Enter' || key === ' ') {
-                        return SelectActions.CloseSelect;
-                    }
-                }
-            }
-        });
-
-        return true;
-    },
+    
 
     installOnMenuKeydown: function (id, objRef) {
         const element = document.querySelector('#' + id + ' ul[role="menu"]');
