3422ff1ef579cd205e935328785088fe517fa436
Mark Baldwin <markb@utopiasoftware.co.uk>
2023-06-22 22:51:11 +0100

    x

---
+++ Utopia.Blazor.Component/HtmlHelpers.cs
@@ -0,0 +1,16 @@
+﻿namespace Utopia.Blazor.Component.Helpers;
+
+public static class HtmlHelpers
+{
+    private static Random random = new();
+    public static string GetRandomHtmlId(int length = 10)
+    {
+        // define the valid characters
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+    }
+    public static string ToHtml(this bool b) => b ? "true" : "false";
+}
---
+++ _Utopia.Blazor.Component/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.Extensions.Logging;
+global using Microsoft.JSInterop;
+global using Utopia.Blazor.Component;
+global using Utopia.Blazor.Component.Helpers;
