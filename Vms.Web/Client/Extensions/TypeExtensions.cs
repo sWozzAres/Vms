cde93c60e26403b4f60e088e65584fce02436720
Mark Baldwin <markb@utopiasoftware.co.uk>
2023-08-05 22:14:46 +0100

    x

--- Vms.Web/Client/Extensions/TypeExtensions.cs
+++ Vms.Web/Client/Extensions/TypeExtensions.cs
@@ -3,4 +3,20 @@
 public static class TypeExtensions
 {
     public static string ToHtml(this bool b) => b ? "true" : "false";
+    public static string ToFriendlyText(this DateTime time)
+    {
+        return $"{FriendlyDateText()} at {time:t}";
+
+        string FriendlyDateText()
+        {
+            if (time.Date == DateTime.Today)
+                return "today";
+            else if (time.Date == DateTime.Today.AddDays(1))
+                return "tommorrow";
+            else if (time.Date == DateTime.Today.AddDays(-1))
+                return "yesterday";
+            else
+                return time.ToString("d");
+        }
+    }
 }
