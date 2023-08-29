491eca9c8ec6a8f65ab6ac7b0dff146df79ec1f5
Mark Baldwin <markb@utopiasoftware.co.uk>
2023-08-29 10:26:47 +0100

    local font

--- Scrum.Api/Application/DomainExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Scrum.Api.Application;
-
-public static class DomainExtensions
-{
-    //public static ProductFullDto ToDto(Product p)
-    //    => new(p.Id, p.Name);
-}
--- Scrum.Api/Application/globalusings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Scrum.Api.Domain;
-global using Scrum.Api.Domain.Infrastructure;
-global using Scrum.Shared;
-global using Microsoft.EntityFrameworkCore;
